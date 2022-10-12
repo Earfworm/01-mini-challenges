637cff89efdd8df127a86730f9dc1f2d863eeb9c
Deshawn Reid <deshawnreid2021@gmail.com>
2022-10-12 09:43:54 -0400

    third commit

--- script.js
+++ script.js
@@ -9,16 +9,28 @@ const gummiesButton = document.querySelector(".gummies");
 let total = 0;
 
 console.dir(totalParagraph);
+const updateVendingTotal = (price) => {
+  total += price;
+  totalParagraph.textContent = `Total: $${total}.00`;
+};
 
 //make money
 
 const coinForm = document.querySelector(".form");
 const coinContainer = document.querySelector(".coin-container");
 
+//light-bulb
+const onButton = document.querySelector(".on");
+const offButton = document.querySelector(".off");
+const toggleButton = document.querySelector(".toggle");
+const endButton = document.querySelector(".end");
+const lightBulb = document.querySelector(".light-bulb")
+const lightButtons = document.querySelectorAll(".light-switch");
 
 colaButton.addEventListener("click", () => {
   total += 2;
   totalParagraph.textContent = `Total: $${total}.00`;
+ 
 });
 peanutsButton.addEventListener("click", () => {
   total += 3;
@@ -32,6 +44,7 @@ gummiesButton.addEventListener("click", () => {
   total += 6;
   totalParagraph.textContent = `Total: $${total}.00`;
 });
+
 
 //2. Make Money
 
@@ -51,15 +64,19 @@ coinForm.addEventListener("submit", (e) => {
     // console.log(i);
     const newCoin = document.createElement("div");
     newCoin.classList.add(typeOfCoins, "coin");
-    if (typeOfCoins === penny) {
-       newCoin.textContent = "Penny";
-    } else if (typeOfCoins === nickel) {
-       newCoin.textContent = "Nickel";
-    } else if (typeOfCoins === dime) {
-      newCoin.textContent = "Dime";
-    } else {
-    newCoin.textContent = "Quarter";
-    }
+    newCoin.textContent = typeOfCoins;
+    // if (typeOfCoins === penny) {
+    //    newCoin.textContent = "Penny";
+    // } else if (typeOfCoins === nickel) {
+    //    newCoin.textContent = "Nickel";
+    // } else if (typeOfCoins === dime) {
+    //   newCoin.textContent = "Dime";
+    // } else {
+    // newCoin.textContent = "Quarter";
+    // }
+    newCoin.addEventListener("click", () => {
+      newCoin.remove();
+    })
     coinContainer.append(newCoin);  
    
   }
@@ -68,4 +85,25 @@ coinForm.addEventListener("submit", (e) => {
 });
  
 
+//light bulb
+
+onButton.addEventListener("click", () => {
+  lightBulb.classList.add("light")
+});
+
+offButton.addEventListener("click", () => {
+  lightBulb.style.backgroundColor = "rgb(70, 70, 70)";
+  lightBulb.style.color = "white";
+});
+
+toggleButton.addEventListener("click", () => {
+  lightBulb.classList.toggle("light");
+});
+
+// endButton.addEventListener("click", () => {
+//   lightBulb.remove();
+//   lightButtons.forEach((button) => {
+//     button.setAttribute("disabled", true);
+//   })
+// });
 
